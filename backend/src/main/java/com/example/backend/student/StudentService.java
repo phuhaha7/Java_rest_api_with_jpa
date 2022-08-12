5ddc5c468cb5192158ea295e2ff59cef38374a2c
Phu Ha <phuthanhha@me.com>
2022-08-12 11:34:59 -0400

    created student model, worked on some service and controller

---
+++ backend/src/main/java/com/example/backend/student/StudentService.java
@@ -0,0 +1,31 @@
+package com.example.backend.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getStudents() {
+        return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> s = studentRepository.findStudentByEmail(student.getEmail());
+        if (s.isPresent()) {
+            throw new RuntimeException("Email taken");
+        }
+        studentRepository.save(student);
+    }
+
+}
